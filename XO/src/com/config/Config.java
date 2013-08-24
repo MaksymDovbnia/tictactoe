4663ffc559b6d7eaf12faaee46501ead77b1e1ca
MaksymDovbnia <maksym.dovbnia@gmail.com>
2013-08-24 12:58:27 +0300

    test

--- XO/src/com/config/Config.java
+++ XO/src/com/config/Config.java
@@ -4,8 +4,13 @@ import android.R.string;
 
 public interface Config {
 	public static final boolean GLOBAL = true;
-	public static final String HOST = (GLOBAL) ? "93.186.200.155"
+
+
+    public static final String HOST = (GLOBAL) ? "93.186.200.155"
 			: "192.168.2.80";
+
+
+
 
 	public static final int PORT = 19999;
 
