60b50d475a947ffa2d549137f97337f76b279571
MaksymDovbnia <maksym.dovbnia@gmail.com>
2013-08-24 12:56:41 +0300

    test

--- XO/src/com/config/Config.java
+++ XO/src/com/config/Config.java
@@ -6,6 +6,7 @@ public interface Config {
 	public static final boolean GLOBAL = true;
 	public static final String HOST = (GLOBAL) ? "93.186.200.155"
 			: "192.168.2.80";
+
 	public static final int PORT = 19999;
 
 }
